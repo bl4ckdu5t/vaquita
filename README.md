9d61d04d37c946366e3d1996e8a929ede2e7a95a
Joseph Rex <joerexko@gmail.com>
2017-06-22 19:51:19 -0500

    Add basic styling with glamorous

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Vaquita
-Vaquita is a open community for design reviews where you can upload designs
+# AlleyHoop
+AlleyHoop is a open community for design reviews where you can upload designs
 and invite anyone to review
 
 ### License
 This project is licensed under the GNU General Public License (GPL-3.0). [Read License][1]
 
-[1]:https://choosealicense.com/licenses/gpl-3.0/
+[1]:https://github.com/josephrexme/alleyhoop/blob/master/LICENSE
--- src/components/view/Home/index.jsx
+++ src/components/view/Home/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import glamorous from 'glamorous';
+import '../../../styles.css';
+
+const Hero = glamorous.div({
+  background: '#caa',
+  padding: '1rem',
+});
+
+const Footer = glamorous.div({
+  background: '#348',
+  padding: '1rem',
+});
 
 const Home = () => (
-  <h1>Home Page</h1>
+  <div>
+    <Hero>
+      <header>
+        <span>Logo</span>
+      </header>
+    </Hero>
+    <Footer>
+      <p>Copyright 2017 Vaquita</p>
+    </Footer>
+  </div>
 );
 
 export default Home;
