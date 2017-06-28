3eaab90cda0ecd680c2f2e0d63f120d28fc30017
Joseph Rex <joerexko@gmail.com>
2017-06-28 10:39:28 -0500

    Create homepage header and basic button subcomponent

---
+++ src/components/subcomponents/Button.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  margin: 0 1rem;
+  padding: 1rem 3rem;
+  background: transparent;
+  color: white;
+  border: solid 2px currentColor;
+  border-radius: 3px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1.6rem;
+  cursor: pointer;
+`;
+
+export default Button;
--- src/components/view/Home/index.jsx
+++ src/components/view/Home/index.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import '../../../styles.css';
 
+import Button from '../../subcomponents/Button';
+
+const brandColor1 = '#238693';
+const brandColor2 = 'hsl(212, 65%, 21%)';
+
 const Hero = styled.div`
-  background: #4caf3d;
+  position: relative;
+  background: linear-gradient(to bottom, ${brandColor1}, ${brandColor2});
+  height: 500px;
   padding: 1rem;
+  color: white;
+  text-align: center;
 `;
 
 const Footer = styled.div`
@@ -12,12 +21,26 @@ const Footer = styled.div`
   padding: 1rem;
 `;
 
+const Shaped = styled.div`
+  position: absolute;
+  left: 0;
+  background: #663399;
+  height: 10vw;
+  width: 100%;
+  transform: skewY(-9deg);
+  opacity: 0;
+`;
+
 const Home = () => (
   <div>
     <Hero>
       <header>
-        <span>Logo</span>
+        <span>AlleyHoop</span>
       </header>
+      <h1>Design reviews for teams and hobbyists</h1>
+      <h2>Invite friends to tell you what they think about your design</h2>
+      <Button>Hi there</Button>
+      <Shaped />
     </Hero>
     <Footer>
       <p>Copyright 2017 Vaquita</p>
